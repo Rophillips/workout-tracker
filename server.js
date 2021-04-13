221d23ab1d57765cd6ad645f6541836b023586e0
Roseanne Phillips <rphillips.rp@gmail.com>
2021-04-12 21:23:36 -0400

    created server

--- server.js
+++ server.js
@@ -2,5 +2,24 @@ const express = require("express");
 const mongoose = require("mongoose");
 const morgan = require("morgan");
 
-const app = express();
+
 const PORT = process.env.PORT || 3000;
+
+const app = express();
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.use(express.static("public"));
+
+//connecting to mongodb atlas
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", {
+    useNewUrlParser: true,
+    useFindAndModify: false
+});
+
+app.listen(PORT, () => {
+    console.log(`App running on port http://localhost:3000`);
+});
+   
+
