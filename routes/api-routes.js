b793c137acc0ed6c750f187c987bd76099b89898
Roseanne Phillips <rphillips.rp@gmail.com>
2021-04-14 17:09:21 -0400

    still working on routes

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -11,7 +11,7 @@ router.get("/api/workouts", (req, res) => {
             }
         }
     ])
-        .then(workout => res.json(workout))
+        .then(dbWorkout => res.json(dbWorkout))
         .catch(err => {res.json (err)});
 });
 
@@ -21,62 +21,32 @@ router.put("/api/workouts/:id", (req, res) => {
            exercises: req.body
        } 
     })
-    .then(workout => res.json(workout))
+    .then(dbWorkout => res.json(dbWorkout))
     .catch(err => {res.json (err)});
 });
 
-router.post("/api/workouts", (req, res) => {
-    
+//create workout
+router.post("/api/workouts", ({ body }, res) => {
+    db.Workout.create(body)
+    .then((dbWorkout => {
+        res.json(dbWorkout);
+    })).catch(err => {res.json (err)});
 });
 
+//get workouts in range
 router.get("/api/workouts/range", (req, res) => {
     
+    db.Workout.find({})
+    .then(dbWorkout => {
+        res.json(dbWorkout)
+        // console.log(" All Workouts");
+        // console.log(dbWorkout);
+    })
+    //.then(workout => )
+    .catch(err => {res.json (err)});
 });
 
-// const API = {
-//     async getLastWorkout() {
-//       let res;
-//       try {
-//         res = await fetch("/api/workouts");
-//       } catch (err) {
-//         console.log(err)
-//       }
-//       const json = await res.json();
-  
-//       return json[json.length - 1];
-//     },
-//     async addExercise(data) {
-//       const id = location.search.split("=")[1];
-  
-//       const res = await fetch("/api/workouts/" + id, {
-//         method: "PUT",
-//         headers: { "Content-Type": "application/json" },
-//         body: JSON.stringify(data)
-//       });
-  
-//       const json = await res.json();
-  
-//       return json;
-//     },
-//     async createWorkout(data = {}) {
-//       const res = await fetch("/api/workouts", {
-//         method: "POST",
-//         body: JSON.stringify(data),
-//         headers: { "Content-Type": "application/json" }
-//       });
-  
-//       const json = await res.json();
-  
-//       return json;
-//     },
-  
-//     async getWorkoutsInRange() {
-//       const res = await fetch(`/api/workouts/range`);
-//       const json = await res.json();
-  
-//       return json;
-//     },
-//   };
+
   
 
 
